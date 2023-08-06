dfa8a4da0e034e03da557b1f44a9857ed35e6fa3
Eve Stroud <evestroud89@gmail.com>
2023-08-06 10:50:43 -0400

    Log in with dotenv and discord.js

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(DISCORD_TOKEN);
