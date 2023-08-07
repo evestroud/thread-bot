02679868911c16a92b50481166b123074b1e8e53
Eve Stroud <evestroud89@gmail.com>
2023-08-07 10:24:07 -0400

    Print thread IDs on /list

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,11 +1,15 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { threads } from "..";
 
 const data = new SlashCommandBuilder()
   .setName("list")
   .setDescription("List active threads");
 const execute = async (interaction: ChatInputCommandInteraction) => {
-  console.log(interaction);
-  interaction.reply("Hi");
+  let output = "";
+  for (const key of threads.keys()) {
+    output += key + "\n";
+  }
+  interaction.reply(output || "No threads found");
 };
 
 export { data, execute };
